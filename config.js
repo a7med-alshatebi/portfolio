679ce8b52c8ee36c66aca444dddda53ce6e8c7cd
Ahmed Alshatebi <ahmedalshatibi22@gmail.com>
2025-07-11 00:13:24 +0300

    aa

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const CONFIG = {
   // Your Google Apps Script web app URL (get this from Step 2 above)
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxskJMWuooZWAAoeef92-0lmXOcOOwgKNItxFUSLxkR3GB9Y5T1IE4EeLiPevQRON1Q9A/exec',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzzjT72_Mw0tpqYUUGCD3dGk4HU7gTMBHRB3PXK6PZlmQgSXPSl2Bzb0kLkHiFfDHNXDg/exec',
   
   // You can add other configuration options here in the future
   CONTACT_EMAIL: 'ahmedalshatibi22@gmail.com'
